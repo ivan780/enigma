76ceae87e44a6df6521080da6b449d5210580578
ivan780 <ivan78082@gmail.com>
2020-05-20 10:06:47 +0200

    Eliminacion de archivo inecesario y añadido a app.js

--- app.js
+++ app.js
@@ -6,12 +6,30 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-//var session = require('express-session');
+var session = require('express-session');
 var mongoose = require('mongoose');
+var debug = require('debug')('enigma:server');
+
 
 //Route required
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+
+
+
+//Init server
+var app = require('express')();
+var server = require('http').createServer(app);
+var io = require('socket.io')(server);
+
+
+//Port
+var port = normalizePort(process.env.PORT || '3000');
+app.set('port', port);
+server.listen(port);
+
+server.on('error', onError);
+server.on('listening', onListening);
 
 
 //Mongose connection
@@ -20,12 +38,16 @@ var db = mongoose.connection;
 db.on('error', console.error.bind(console, "MongoDB conection error:"))
 
 
-//Init app
-var app = express();
+//socketIo
+io.on('connection', (socket) => {
+    console.log('a user connected');
+});
+
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
+
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -36,7 +58,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 //Routes
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-
 
 
 // catch 404 and forward to error handler
@@ -56,4 +77,61 @@ app.use(function (err, req, res, next) {
     res.render('error');
 });
 
-module.exports = app;
+/**
+ * Normalize a port into a number, string, or false.
+ */
+function normalizePort(val) {
+    var port = parseInt(val, 10);
+
+    if (isNaN(port)) {
+        // named pipe
+        return val;
+    }
+
+    if (port >= 0) {
+        // port number
+        return port;
+    }
+
+    return false;
+}
+
+/**
+ * Event listener for HTTP server "error" event.
+ */
+
+function onError(error) {
+    if (error.syscall !== 'listen') {
+        throw error;
+    }
+
+    var bind = typeof port === 'string'
+        ? 'Pipe ' + port
+        : 'Port ' + port;
+
+    // handle specific listen errors with friendly messages
+    switch (error.code) {
+        case 'EACCES':
+            console.error(bind + ' requires elevated privileges');
+            process.exit(1);
+            break;
+        case 'EADDRINUSE':
+            console.error(bind + ' is already in use');
+            process.exit(1);
+            break;
+        default:
+            throw error;
+    }
+}
+
+/**
+ * Event listener for HTTP server "listening" event.
+ */
+
+function onListening() {
+    var addr = server.address();
+    var bind = typeof addr === 'string'
+        ? 'pipe ' + addr
+        : 'port ' + addr.port;
+    debug('Listening on ' + bind);
+}
