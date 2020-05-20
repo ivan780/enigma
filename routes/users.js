ac4b40ac7ed11ce0c9f3c38137e5b9f47c7a6218
ivan780 <ivan78082@gmail.com>
2020-05-20 17:21:31 +0200

    add route for change password

--- routes/users.js
+++ routes/users.js
@@ -3,9 +3,9 @@ var router = express.Router();
 
 var user_controller = require('../controllers/userController')
 
-router.get('/', function (req, res) {
-    res.render('index');
-})
+router.get('/', user_controller.index);
+
+
 router.get('/login', user_controller.login);
 
 router.post('/login', user_controller.loginPOST);
@@ -13,5 +13,11 @@ router.post('/login', user_controller.loginPOST);
 router.get('/reg', user_controller.createUser);
 
 router.post('/reg', user_controller.createUserPOST);
+
+router.get('/change-password', user_controller.changePass);
+
+router.post('/change-password', user_controller.changePassPOST);
+
+
 
 module.exports = router;
