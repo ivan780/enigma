7c42c3655aee5699806e92ed22bb7c8822eafce7
ivan780 <ivan78082@gmail.com>
2020-05-21 19:41:56 +0200

    move fuction

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,21 @@
 var express = require('express');
 var router = express.Router();
 
-router.get('/test', function (req, res) {
-    res.render('login')
-});
+var user_controller = require('../controllers/userController')
+
+
+router.get('/dashboard', user_controller.index);
+
+router.get('/login', user_controller.login);
+
+router.post('/login', user_controller.loginPOST);
+
+router.get('/reg', user_controller.createUser);
+
+router.post('/reg', user_controller.createUserPOST);
+
+router.get('/change-password', user_controller.changePass);
+
+router.post('/change-password', user_controller.changePassPOST);
 
 module.exports = router;
