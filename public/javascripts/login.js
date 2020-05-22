e47e3d6de16f8d05f82281a02415abee5acf040c
ivan780 <ivan78082@gmail.com>
2020-05-22 19:30:04 +0200

    script for check if username or email exist in the database with socketIo

---
+++ public/javascripts/login.js
@@ -0,0 +1,42 @@
+console.log("init")
+var socket = io.connect('http://localhost:3000');
+
+var submitBtt = document.getElementById("submitBtt")
+submitBtt.disabled = true
+
+var username = document.getElementById("userName").innerText;
+
+var inputUsername = document.getElementById("username");
+var inputEmail = document.getElementById("email");
+
+if (inputEmail){
+    inputEmail.addEventListener('input', updateEmail);
+}
+if (inputUsername){
+    inputUsername.addEventListener('input', updateUsername);
+}
+
+function updateUsername(e) {
+    socket.emit('checkUsername', {
+        payload: e.target.value
+    }, function (responseData) {
+        disabled(responseData)
+    })
+}
+
+function updateEmail(e) {
+    if(e.target.value === username){
+        return disabled(true);
+    }
+    console.log(document.getElementById("userName").innerText)
+    socket.emit('checkEmail', {
+        payload: e.target.value
+
+    }, function (responseData) {
+        disabled(responseData)
+    })
+}
+
+function disabled(state) {
+    submitBtt.disabled = state
+}
