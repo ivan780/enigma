934975594c6cce6fe6f157b965b04e72709ae0e7
ivan780 <ivan78082@gmail.com>
2020-05-26 10:55:20 +0200

    function for check if the id is correct

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,14 +1,13 @@
 const express = require('express');
 const bcrypt = require('bcryptjs');
-//const jwt = require('jsonwebtoken');
 const Usuario = require('./../models/usuario');
 const session = require('express-session');
 var jwt = require('../auth/local')
 var md5 = require('md5')
 
 exports.index = function (req, res, next) {
-    var listCont=[];
-    var urlCont=[];
+    var listCont = [];
+    var urlCont = [];
     if (req.cookies["token"]) {
         jwt.decodeToken(req.cookies["token"], function (msg, data) {
                 if (msg) {
@@ -16,12 +15,12 @@ exports.index = function (req, res, next) {
                 }
                 Usuario.findOne({email: data.sub}).then(function (user) {
                     var contactos = user.contactos;
-                    for (var i=0;i<contactos.length;i++){
+                    for (var i = 0; i < contactos.length; i++) {
                         listCont.push(contactos[i].split("///")[0]);
-                        if (contactos[i].split("///")[1] === 2){
-                            urlCont.push(md5(user.email+listCont[i]));
-                        }else {
-                            urlCont.push(md5(listCont[i]+user.email))
+                        if (contactos[i].split("///")[1] === 2) {
+                            urlCont.push(md5(user.email + listCont[i]));
+                        } else {
+                            urlCont.push(md5(listCont[i] + user.email))
                         }
                     }
                     console.log(urlCont)
@@ -86,7 +85,7 @@ exports.addContact = function (req, res, next) {
                 if (err) {
                     return next(err);
                 }
-                Usuario.findOne({email: data.sub}).then(function (user) {
+                Usuario.findOne({email: req.body.email}).then(function (user) {
                     //save contact for the other contact
                     user.contactos.push(data.sub + "///1")
                     user.save(function (err) {
@@ -124,24 +123,29 @@ exports.changePassPOST = function (req, res, next) {
     });
 };
 
-/**
- exports.delete = function (req, res, next) {
-    res.render('user/signIn');
-};
-
- exports.deletePOST = function (req, res, next) {
-    console.log(req.body.email + "//" + req.body.oldPass);
-    var usuario = Usuario.findOne({email: req.body.email}, function (err, user) {
-        if (!user || !bcrypt.compareSync(req.body.oldPass, user.password)) {
-            res.render('signIn', {email: req.body.email, errors: "Email o contraseña incorrecto"});
-            return;
+exports.chat = function (req, res, next) {
+    jwt.decodeToken(req.cookies["token"], function (msg, data) {
+        if (msg) {
+            return res.redirect('/dashboard');
         }
-        if (req.body.newPass) {
-            user.password = req.body.newPass;
-            user.save;
-        }
-        // Successful - redirect to new author record.
-        res.redirect('/users/login');
+        Usuario.findOne({email: data.sub}).then(function (user) {
+            for (var i = 0; i < user.contactos.length; i++) {
+                var idPos
+                console.log(user.contactos[i].split("///"))
+                if (user.contactos[i].split("///")[1] === '2') {
+                    console.log("hola")
+                    idPos = md5(user.email + user.contactos[i].split("///")[0])
+                } else {
+                    console.log("adios")
+                    idPos = md5( user.contactos[i].split("///")[0] + user.email)
+                }
+                console.log("idPos: " + idPos + "///" + req.params.id)
+                if (req.params.id === idPos) {
+                    console.log("maricon")
+                    return res.render('chat');
+                }
+            }
+            return res.redirect('/dashboard');
+        });
     });
-};
- **/
+}
